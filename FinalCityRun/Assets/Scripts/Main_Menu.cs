ad82f989d9a25277ba916ea36ec189c0c716af2f
Bipin <bipinchhetri18@gmail.com>
2022-05-03 14:03:18 +0545

    UI/UX Test

--- FinalCityRun/Assets/Scripts/Main_Menu.cs
+++ FinalCityRun/Assets/Scripts/Main_Menu.cs
@@ -1,24 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
-
-    
-    // Start is called before the first frame update
+    public Text HighscoreText;
     void Start()
     {
-        
+        HighscoreText.text = "HighScore : " + ((int)PlayerPrefs.GetFloat("HighScore")).ToString();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void ToMenu()
     {
         SceneManager.LoadScene("MainScene");
